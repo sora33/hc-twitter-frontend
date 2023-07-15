e4dc5597c9e29757db8ac3f8c0eb537ae6a1f979
hiranuma <shuuuya0616@gmail.com>
2023-07-15 11:53:59 +0900

    初期設定

---
+++ README.md
@@ -0,0 +1,6 @@
+
+## プロダクトの立ち上げ
+yarn create vite
+yarn add react-router-dom @types/react-router-dom
+yarn add react-hook-form
+yarn add @chakra-ui/react @emotion/react @emotion/styled framer-motion
---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Text } from "@chakra-ui/react"
+import { BrowserRouter } from "react-router-dom"
+
+export const App: React.FC = () => {
+  return (
+    <BrowserRouter>
+      <Text>テキストテキスト</Text>
+    </BrowserRouter>
+  )
+}
