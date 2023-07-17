a5253e11be14534762a02416aafc2a61abc586e5
hiranuma <shuuuya0616@gmail.com>
2023-07-17 12:47:16 +0900

    axiosの設定を定義

---
+++ src/lib/axios/apiClient.ts
@@ -0,0 +1,14 @@
+import axios from "axios";
+import applyCaseMiddleware from "axios-case-converter";
+
+// ヘッダーに関してはケバブケースのままで良いので適用を無視するオプションを追加
+const options = {
+  ignoreHeaders: true,
+};
+
+export const apiClient = applyCaseMiddleware(
+  axios.create({
+    baseURL: `${import.meta.env.VITE_BACKEND_ENDPOINT as string}/api/v1`,
+  }),
+  options
+);
