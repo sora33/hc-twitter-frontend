ad5a295fe6664c98c88bca493669a754162f96fb
hiranuma <shuuuya0616@gmail.com>
2023-07-20 11:01:42 +0900

    ホームページを作成

---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+import { HeadingH1 } from "components/heading/HeadingH1";
+
+export const Home: React.FC = () => {
+  return (
+    <>
+      <HeadingH1>ホームページ（ログイン後のみ）</HeadingH1>
+    </>
+  );
+};
