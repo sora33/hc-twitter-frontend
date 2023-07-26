23ea931c1aba5f332dc3c08a6b9fa77ef7a493c0
hiranuma <shuuuya0616@gmail.com>
2023-07-26 16:21:49 +0900

    サイドバーにアイコンを追加

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -1,20 +1,49 @@
 import { Stack } from "@chakra-ui/react";
 import { MainButton } from "components/button/MainButton";
 import { useLocation } from "react-router-dom";
+import {
+  AiOutlineHome,
+  AiOutlineBell,
+  AiOutlineMail,
+  AiOutlineVerticalAlignBottom,
+  AiOutlineUser,
+} from "react-icons/ai";
+
+const iconStyle = { marginTop: "-4px" };
 
 const SidebarList = [
-  { name: "ホーム", link: "/home" },
-  { name: "通知", link: "/notification" },
-  { name: "メッセージ", link: "/message" },
-  { name: "ブックマーク", link: "/bookmark" },
-  { name: "プロフィール", link: "/profile" },
+  {
+    name: "ホーム",
+    link: "/home",
+    icon: <AiOutlineHome style={iconStyle} />,
+  },
+  {
+    name: "通知",
+    link: "/notification",
+    icon: <AiOutlineBell style={iconStyle} />,
+  },
+  {
+    name: "メッセージ",
+    link: "/message",
+    icon: <AiOutlineMail style={iconStyle} />,
+  },
+  {
+    name: "ブックマーク",
+    link: "/bookmark",
+    icon: <AiOutlineVerticalAlignBottom style={iconStyle} />,
+  },
+  {
+    name: "プロフィール",
+    link: "/profile",
+    icon: <AiOutlineUser style={iconStyle} />,
+  },
 ];
 
 export const Sidebar: React.FC = () => {
   const location = useLocation();
 
   return (
-    <Stack as="nav" spacing={4} w="250px">
+    <Stack as="nav" spacing={4} w="180px">
       {SidebarList.map((item) => (
         <MainButton
           link={item.link}
@@ -22,6 +51,7 @@ export const Sidebar: React.FC = () => {
           colorScheme="gray"
           variant={location.pathname === item.link ? "solid" : "ghost"}
           key={item.name}
+          leftIcon={item.icon}
         >
           {item.name}
         </MainButton>
