a277f1273c1ffa8277e5e3a999443d48ac7b51ca
hiranuma <shuuuya0616@gmail.com>
2023-07-26 16:34:44 +0900

    アバターを追加

--- src/features/tweet/views/PostTweetForm.tsx
+++ src/features/tweet/views/PostTweetForm.tsx
@@ -7,6 +7,7 @@ import { useToastMessage } from "hooks/useToastMessage";
 import { useState, useEffect } from "react";
 import { postTweet } from "features/tweet/tweetApis";
 import { AiFillPicture, AiFillCloseCircle } from "react-icons/ai";
+import { MainAvatar } from "components/avatar/MainAvatar";
 
 const checkFileSize = (value: FileList | null) => {
   const MAX_FILE_SIZE = 2 * 1024 * 1024;
@@ -60,71 +61,69 @@ export const PostTweetForm: React.FC = () => {
   };
 
   return (
-    <Stack as="form" spacing="4" w="100%" onSubmit={handleSubmit(onSubmit)}>
-      <FormInput
-        placeholder="今どうしてる？"
-        type="textarea"
-        register={register("content", {
-          required: "必須項目です",
-          maxLength: { value: 140, message: "140文字以内で入力してください" },
-        })}
-        error={errors.content?.message}
-      />
-      {previewUrl && (
-        <Box position="relative">
-          <Image src={previewUrl} alt="preview" rounded="2xl" />
-          <Icon
-            onClick={onRemoveImage}
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-            as={AiFillCloseCircle}
-            cursor="pointer"
-            position="absolute"
-            top="-1rem"
-            right="-1rem"
-            w="8"
-            h="8"
-          />
-        </Box>
-      )}
+    <Flex pb="2" borderBottom="1px solid" borderColor="gray.200">
+      <MainAvatar mr="4" />
+      <Stack as="form" spacing="2" w="100%" onSubmit={handleSubmit(onSubmit)}>
+        <FormInput
+          placeholder="今どうしてる？"
+          type="textarea"
+          register={register("content", {
+            required: "必須項目です",
+            maxLength: { value: 140, message: "140文字以内で入力してください" },
+          })}
+          error={errors.content?.message}
+        />
+        {previewUrl && (
+          <Box position="relative">
+            <Image src={previewUrl} alt="preview" rounded="2xl" />
+            <Icon
+              onClick={onRemoveImage}
+              // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+              as={AiFillCloseCircle}
+              cursor="pointer"
+              position="absolute"
+              top="-1rem"
+              right="-1rem"
+              w="8"
+              h="8"
+            />
+          </Box>
+        )}
 
-      <Flex
-        justifyContent="space-between"
-        alignItems="center"
-        pb="2"
-        borderBottom="1px solid"
-        borderColor="gray.200"
-      >
-        <Box>
-          <FormInput
-            type="file"
-            label={
-              <Icon
-                // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-                as={AiFillPicture}
-                mt="2"
-                w={7}
-                h={7}
-                color="blue.500"
-                cursor="pointer"
-              />
-            }
-            register={register("image", {
-              validate: {
-                checkFileSize,
-              },
-            })}
-            error={errors.image?.message}
-          />
-        </Box>
-        <MainButton
-          type="submit"
-          variant="solid"
-          isLoading={isLoading}
-          size="sm"
-        >
-          ツイートする
-        </MainButton>
-      </Flex>
-    </Stack>
+        <Flex justifyContent="space-between" alignItems="center">
+          <Box>
+            <FormInput
+              type="file"
+              label={
+                <Icon
+                  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+                  as={AiFillPicture}
+                  mt="2"
+                  w={7}
+                  h={7}
+                  color="blue.500"
+                  cursor="pointer"
+                />
+              }
+              register={register("image", {
+                validate: {
+                  checkFileSize,
+                },
+              })}
+              error={errors.image?.message}
+            />
+          </Box>
+          <MainButton
+            type="submit"
+            variant="solid"
+            isLoading={isLoading}
+            size="sm"
+            rounded="3xl"
+          >
+            ツイートする
+          </MainButton>
+        </Flex>
+      </Stack>
+    </Flex>
   );
 };
