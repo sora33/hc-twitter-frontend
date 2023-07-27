25fbb5f80577aa2b0d84f184ca1c4da518e4f024
hiranuma <shuuuya0616@gmail.com>
2023-07-27 12:12:38 +0900

    ツイート作成時に、ツイート一覧を更新するロジックを追加

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,23 @@
+import { useState, createContext } from "react";
 import { PostTweetForm } from "features/tweet/views/PostTweetForm";
+import { TweetList } from "features/tweet/views/TweetList";
 import { HeadingH1 } from "components/heading/HeadingH1";
+
+export const TweetContext = createContext({
+  refreshTweets: false,
+  setRefreshTweets: (refresh: boolean) => {
+    refresh;
+  },
+});
+
 export const Home: React.FC = () => {
+  const [refreshTweets, setRefreshTweets] = useState(false);
+
   return (
-    <>
+    <TweetContext.Provider value={{ refreshTweets, setRefreshTweets }}>
       <HeadingH1>ホーム</HeadingH1>
       <PostTweetForm />
-    </>
+      <TweetList />
+    </TweetContext.Provider>
   );
 };
