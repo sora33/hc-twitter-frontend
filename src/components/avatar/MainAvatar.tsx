aa98dec3f2f8bd37c482033ad8755cf453ded2ec
hiranuma <shuuuya0616@gmail.com>
2023-08-01 23:19:12 +0900

    アバターを共通化

--- src/components/avatar/MainAvatar.tsx
+++ src/components/avatar/MainAvatar.tsx
@@ -1,7 +1,21 @@
 import { Avatar, AvatarProps } from "@chakra-ui/react";
-
-type MainAvatarProps = AvatarProps;
+import { Link as RouterLink } from "react-router-dom";
+type MainAvatarProps = AvatarProps & {
+  link?: string;
+};
 
 export const MainAvatar: React.FC<MainAvatarProps> = (props) => {
+  const { link } = props;
+  if (link) {
+    return (
+      <Avatar
+        size="sm"
+        as={RouterLink}
+        to={link}
+        {...props}
+        _hover={{ opacity: 0.8 }}
+      />
+    );
+  }
   return <Avatar size="sm" {...props} />;
 };
