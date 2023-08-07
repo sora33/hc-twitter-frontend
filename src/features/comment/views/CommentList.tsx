19fa64c944d34563d090f7d883516de927bf61db
hiranuma <shuuuya0616@gmail.com>
2023-08-07 16:26:11 +0900

    コンポーネントを分割

--- src/features/comment/views/CommentList.tsx
+++ src/features/comment/views/CommentList.tsx
@@ -1,8 +1,7 @@
-import { Flex, HStack, Text, Stack, Box } from "@chakra-ui/react";
-import { MainAvatar } from "components/avatar/MainAvatar";
-import { formatDate } from "lib/functions/formatDate";
-import { MainImage } from "components/image/MainImage";
+import { Text, Box } from "@chakra-ui/react";
+
 import { Comment } from "features/comment/commentTypes";
+import { CommentCard } from "features/comment/views/CommentCard";
 
 type commentListProps = {
   comments: Comment[];
@@ -15,29 +14,10 @@ export const CommentList: React.FC<commentListProps> = ({ comments }) => {
         コメント一覧
       </Text>
       {comments?.length > 0 ? (
-        comments?.map((comment) => (
-          <Flex borderBottom="1px solid" borderColor="gray.200" py="2">
-            <MainAvatar
-              mr="4"
-              src={comment.user.avatarImage ?? ""}
-              link={`/users/${comment.user.id}`}
-            />
-            <Stack position="relative" spacing="2" flex="1">
-              <Stack spacing="2">
-                <HStack>
-                  <Text fontWeight="bold">{comment.user.name}</Text>
-                  <Text size="sm" color="gray">
-                    {formatDate(comment.createdAt)}
-                  </Text>
-                </HStack>
-                <Text whiteSpace="pre-line" lineHeight="shorter">
-                  {comment.content}
-                </Text>
-                {comment.image && <MainImage src={comment.image} />}
-              </Stack>
-            </Stack>
-          </Flex>
-        ))
+        comments?.map((commentItem) => {
+          const { user, ...comment } = commentItem;
+          return <CommentCard comment={comment} user={user} />;
+        })
       ) : (
         <Text fontSize="sm">コメントはありません</Text>
       )}
