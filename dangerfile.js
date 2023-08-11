4344a6c83226b34fcfa9d2b1790867c53d843dab
hiranuma <shuuuya0616@gmail.com>
2023-08-12 04:55:47 +0900

    diffLinesのロジック修正

--- dangerfile.js
+++ dangerfile.js
@@ -2,16 +2,18 @@ import { danger, warn } from "danger";
 
 const modifiedFiles = danger.git.modified_files;
 
-const addedLines = danger.git.created_files.reduce(
-  (acc, file) => acc + (danger.git.diffForFile(file).added || "").split("\n").length,
-  0
-);
-const deletedLines = danger.git.deleted_files.reduce(
-  (acc, file) => acc + (danger.git.diffForFile(file).deleted || "").split("\n").length,
-  0
-);
+// const addedLines = danger.git.created_files.reduce(
+//   (acc, file) => acc + (danger.git.diffForFile(file).added || "").split("\n").length,
+//   0
+// );
+// const deletedLines = danger.git.deleted_files.reduce(
+//   (acc, file) => acc + (danger.git.diffForFile(file).deleted || "").split("\n").length,
+//   0
+// );
 
-if (addedLines + deletedLines > 2) {
+const diffLines = danger.github.pr.additions + danger.github.pr.deletions;
+
+if (diffLines > 2) {
   warn("You have more than 200 line changes in this PR");
 }
 
