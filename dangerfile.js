074acc873e846038a4bf511941a0b2d1b413bb4e
hiranuma <shuuuya0616@gmail.com>
2023-08-12 04:45:10 +0900

    dangerfileを修正

--- dangerfile.js
+++ dangerfile.js
@@ -1,26 +1,24 @@
 import { danger, fail, warn } from "danger";
 
+const modifiedFiles = danger.git.modified_files;
+
+const addedLines = danger.git.created_files.reduce(
+  (acc, file) => acc + (danger.git.diffForFile(file).added || "").split("\n").length,
+  0
+);
+const deletedLines = danger.git.deleted_files.reduce(
+  (acc, file) => acc + (danger.git.diffForFile(file).deleted || "").split("\n").length,
+  0
+);
+
+if (addedLines + deletedLines > 200) {
+  warn("You have more than 200 line changes in this PR");
+}
+
+if (modifiedFiles.length > 10) {
+  warn("You have modified more than 10 files in this PR");
+}
+
 if (!danger.github.pr.assignee) {
   fail("This pull request needs an assignee, and optionally include any reviewers.");
 }
-
-// import { danger, warn } from "danger";
-
-// const modifiedFiles = danger.git.modified_files;
-
-// const addedLines = danger.git.created_files.reduce(
-//   (acc, file) => acc + (danger.git.diffForFile(file).added || "").split("\n").length,
-//   0
-// );
-// const deletedLines = danger.git.deleted_files.reduce(
-//   (acc, file) => acc + (danger.git.diffForFile(file).deleted || "").split("\n").length,
-//   0
-// );
-
-// if (addedLines + deletedLines > 200) {
-//   warn("You have more than 200 line changes in this PR");
-// }
-
-// if (modifiedFiles.length > 10) {
-//   warn("You have modified more than 10 files in this PR");
-// }
