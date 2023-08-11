1409732558e1b3d4a2066fa99774c99222eb4a73
hiranuma <shuuuya0616@gmail.com>
2023-08-11 12:47:03 +0900

    dangerfile.jsを追加

---
+++ dangerfile.js
@@ -0,0 +1,20 @@
+import { danger, warn } from "danger";
+
+const modifiedFiles = danger.git.modified_files;
+
+const addedLines = danger.git.created_files.reduce(
+  (acc, file) => acc + (danger.git.diffForFile(file).added || "").split("\n").length,
+  0
+);
+const deletedLines = danger.git.deleted_files.reduce(
+  (acc, file) => acc + (danger.git.diffForFile(file).deleted || "").split("\n").length,
+  0
+);
+
+if (addedLines + deletedLines > 200) {
+  warn("You have more than 200 line changes in this PR");
+}
+
+if (modifiedFiles.length > 10) {
+  warn("You have modified more than 10 files in this PR");
+}
