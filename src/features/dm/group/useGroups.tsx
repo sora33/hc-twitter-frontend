ac0407fa899e7cfc2af2aa62d3ce4bfe6cd81996
hiranuma <shuuuya0616@gmail.com>
2023-09-05 10:34:01 +0900

    不要な記述を削除

--- src/features/dm/group/useGroups.tsx
+++ src/features/dm/group/useGroups.tsx
@@ -1,41 +1,35 @@
 import { getGroups } from "features/dm/group/groupApis";
-import { useEffect, useReducer, useState } from "react";
+import { useEffect, useReducer } from "react";
 import { Group } from "features/dm/group/groupTypes";
 
 const initialState = {
   data: null,
   isLoading: false,
   error: null,
-  isRefetching: false,
 };
 const FETCH_START = "FETCH_START";
 const FETCH_SUCCESS = "FETCH_SUCCESS";
 const FETCH_ERROR = "FETCH_ERROR";
-const REFETCH_START = "REFETCH_START";
 
 type State = {
   data: Group[] | null;
   isLoading: boolean;
   error: Error | null;
-  isRefetching: boolean;
 };
 
 type Action =
   | { type: typeof FETCH_START }
   | { type: typeof FETCH_SUCCESS; payload: Group[] }
-  | { type: typeof FETCH_ERROR; payload: Error | null }
-  | { type: typeof REFETCH_START };
+  | { type: typeof FETCH_ERROR; payload: Error | null };
 
 const reducer = (state: State, action: Action): State => {
   switch (action.type) {
     case FETCH_START:
       return { ...state, isLoading: true };
     case FETCH_SUCCESS:
-      return { ...state, isLoading: false, isRefetching: false, data: action.payload };
+      return { ...state, isLoading: false, data: action.payload };
     case FETCH_ERROR:
-      return { ...state, isLoading: false, isRefetching: false, error: action.payload };
-    case REFETCH_START:
-      return { ...state, isRefetching: true };
+      return { ...state, isLoading: false, error: action.payload };
     default:
       throw new Error("no such action type!");
   }
@@ -43,30 +37,21 @@ const reducer = (state: State, action: Action): State => {
 
 export const useGroups = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
-  const [refetch, setRefetch] = useState(false);
 
-  const fetchGroups = async (isRefetch: boolean) => {
-    dispatch({ type: isRefetch ? REFETCH_START : FETCH_START });
+  const fetchGroups = async () => {
+    dispatch({ type: FETCH_START });
     try {
       const res = await getGroups();
       dispatch({ type: FETCH_SUCCESS, payload: res });
     } catch (error) {
       console.log(error);
       dispatch({ type: FETCH_ERROR, payload: error as Error });
-    } finally {
-      if (isRefetch) setRefetch(false);
     }
   };
 
   useEffect(() => {
-    void fetchGroups(false);
+    void fetchGroups();
   }, []);
 
-  useEffect(() => {
-    if (refetch) {
-      void fetchGroups(true);
-    }
-  }, [refetch]);
-
-  return { ...state, setRefetch };
+  return { ...state };
 };
--- src/features/dm/message/useMessages.tsx
+++ src/features/dm/message/useMessages.tsx
@@ -1,5 +1,5 @@
 import { getMessages } from "features/dm/message/messageApis";
-import { useEffect, useReducer, useState } from "react";
+import { useEffect, useReducer } from "react";
 import { Message } from "features/dm/message/messageTypes";
 import { useParams } from "react-router-dom";
 
@@ -7,36 +7,30 @@ const initialState = {
   data: null,
   isLoading: false,
   error: null,
-  isRefetching: false,
 };
 const FETCH_START = "FETCH_START";
 const FETCH_SUCCESS = "FETCH_SUCCESS";
 const FETCH_ERROR = "FETCH_ERROR";
-const REFETCH_START = "REFETCH_START";
 
 type State = {
   data: Message[] | null;
   isLoading: boolean;
   error: Error | null;
-  isRefetching: boolean;
 };
 
 type Action =
   | { type: typeof FETCH_START }
   | { type: typeof FETCH_SUCCESS; payload: Message[] }
-  | { type: typeof FETCH_ERROR; payload: Error | null }
-  | { type: typeof REFETCH_START };
+  | { type: typeof FETCH_ERROR; payload: Error | null };
 
 const reducer = (state: State, action: Action): State => {
   switch (action.type) {
     case FETCH_START:
       return { ...state, isLoading: true };
     case FETCH_SUCCESS:
-      return { ...state, isLoading: false, isRefetching: false, data: action.payload };
+      return { ...state, isLoading: false, data: action.payload };
     case FETCH_ERROR:
-      return { ...state, isLoading: false, isRefetching: false, error: action.payload };
-    case REFETCH_START:
-      return { ...state, isRefetching: true };
+      return { ...state, isLoading: false, error: action.payload };
     default:
       throw new Error("no such action type!");
   }
@@ -44,31 +38,21 @@ const reducer = (state: State, action: Action): State => {
 
 export const useMessages = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
-  const [refetch, setRefetch] = useState(false);
   const { id } = useParams<{ id: string }>();
 
-  const fetchMessages = async (isRefetch: boolean) => {
-    dispatch({ type: isRefetch ? REFETCH_START : FETCH_START });
+  const fetchMessages = async () => {
     try {
       const res = await getMessages(id!);
       dispatch({ type: FETCH_SUCCESS, payload: res });
     } catch (error) {
       console.log(error);
       dispatch({ type: FETCH_ERROR, payload: error as Error });
-    } finally {
-      if (isRefetch) setRefetch(false);
     }
   };
 
   useEffect(() => {
-    void fetchMessages(false);
-  }, []);
+    void fetchMessages();
+  }, [id]);
 
-  useEffect(() => {
-    if (refetch) {
-      void fetchMessages(true);
-    }
-  }, [refetch]);
-
-  return { ...state, setRefetch };
+  return { ...state };
 };
